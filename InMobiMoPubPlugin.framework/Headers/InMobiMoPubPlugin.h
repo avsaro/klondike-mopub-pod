537d5252f7f8195bd979072d9507cf8b3e6dd98a
Onur Avsar <oavsar@zynga.com>
2020-07-08 17:40:32 +0300

    Updated inmobi to v9.0.7

---
+++ InMobiMoPubPlugin.framework/Headers/InMobiMoPubPlugin.h
@@ -0,0 +1,18 @@
+//
+//  InMobiMoPubPlugin.h
+//  UnifiedSDK
+//
+//  Copyright © 2020 InMobi. All rights reserved.
+//
+
+#ifndef InMobiMoPubPlugin_h
+#define InMobiMoPubPlugin_h
+
+#import "IMABMoPubInterstitial.h"
+#import "IMABMoPubBanner.h"
+#import "IMABMoPubDelegate.h"
+#import "IMFacadeWrapper.h"
+#import "InMobiGDPR.h"
+#import "InMobiSDKInitialiser.h"
+
+#endif /* InMobiMoPubPlugin_h */
